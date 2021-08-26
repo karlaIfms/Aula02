782bfdae3aad4a91efedac1b66b82c176a10dcaf
karla Lirio <karlalirio12@gmail.com>
2021-08-26 15:57:50 -0300

    Incluindo ícones

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hellow world!</Text>
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+        <View style={styles.header}>
+            <Image source={
+                require("./src/assets/arrow-left.png")
+                }>
+            </Image>
+            <Image source={
+                require("./src/assets/ei_search.png")
+                }>
+            </Image>
+            <Image source={
+                require("./src/assets/share.png")
+                }>
+            </Image>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
+    header:{
+        minHeight:70,
+        backgroundColor:"#4369B0",
+        paddingVertical:30,
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        paddingHorizontal:30,
+    },
 });
